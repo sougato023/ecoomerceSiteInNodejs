eda845a091cec45065a20df2849824e07037b90e
Sougato <sougato.chakraborty@sap.com>
2020-11-05 21:22:43 +0530

    Latest changes

---
+++ ecommerce-front/src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+const App = () => {
+  return (
+    <h2>Welcome to React App</h2>
+  );
+}
+
+
+
+export default App;
---
+++ ecommerce-front/src/user/Routes.js
@@ -0,0 +1,19 @@
+import React from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Signup from "./Signup"
+import Signin from "./Signin"
+import Home from "../core/Home"
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/signup" exact component={Signup}/>
+                <Route path="/signin" exact component={Signin}/>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
